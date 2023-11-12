32ff7a00e81b1b896baff4f62b6496733e2ea90b
Johanne <e-johher1@iskule.no>
2023-11-12 11:20:23 +0100

    fixed function name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SocialLinks from "../components/socials-links";
 
-export default function () {
+export default function HomePage() {
   return (
     <div className="m-10">
       <SocialLinks />
