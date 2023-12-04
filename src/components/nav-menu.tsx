8463094dad3e8945cc6806746c5edf6e896cc8a3
Johanne <e-johher1@iskule.no>
2023-12-04 21:58:51 +0100

    fixed layout

---
+++ src/components/nav-menu.tsx
@@ -0,0 +1,63 @@
+"use state";
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+export function NavMenu() {
+  return (
+    <div className="flex gap-3">
+      <Button variant="secondary">
+          <Link className="flex justify-between hover:underline decoration-1 underline-offset-2 w-auto" href="/projects">
+            Prosjekter
+          </Link>
+        </Button>
+        <Button variant="secondary">
+          <Link className="flex justify-between hover:underline decoration-1 underline-offset-2 w-auto" href="/games">
+            Spill
+          </Link>
+        </Button>
+    </div>
+  );
+}
+
+interface DropDownNavMenuProps {
+  onClose: () => void;
+}
+
+export function DropDownNavMenu({ onClose }: DropDownNavMenuProps) {
+  return (
+    <nav className="absolute right-0">
+      <div className="flex flex-col gap-2 p-2 mt-1">
+        <Button variant="secondary" onClick={onClose}>
+          <Link className="flex justify-between hover:underline decoration-1 underline-offset-2 w-auto" href="/projects">
+            Prosjekter
+          </Link>
+        </Button>
+        <Button variant="secondary" onClick={onClose}>
+          <Link className="flex justify-between hover:underline decoration-1 underline-offset-2 w-auto" href="/games">
+            Spill
+          </Link>
+        </Button>
+      </div>
+    </nav>
+  );
+}
+
+export function NavBurger() {
+  const [burger, setBurger] = useState(false);
+
+  const toggleBurger = () => {
+    setBurger(!burger);
+  };
+
+  return (
+    <div>
+      <Button size="icon" variant="secondary" onClick={toggleBurger}>
+        <Menu />
+      </Button>
+      {burger ? <DropDownNavMenu onClose={toggleBurger} /> : null}
+    </div>
+  );
+}
