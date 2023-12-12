cb97dcf8436a27bf9529900690fff46c77ce0f01
Johanne <joher2128@uib.no>
2023-12-12 19:54:12 +0100

    added picture

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import Image from "next/image";
+import me from "src/components/images/me.jpeg";
 
 export default function HomePage() {
   return (
-    <div className="m-10 sm:mt-20 w-auto sm:w-1/3 flex flex-col p-2 my-auto">
-      <h2 className="pb-5 border-b">Titthei 👋🏼</h2>
-      <p className="pt-3">
-        Mitt navn er Johanne, og eg studerer informatikk; datatryggleik ved
-        Universitetet i Bergen 👩🏼‍💻.
-      </p>
+    <div className="m-16 sm:mt-20 w-auto sm:w-2/3 flex p-2 my-auto gap-10">
+      <div className="overflow-hidden rounded-full border h-40 w-40 hidden sm:block flex-none">
+        <Image
+          src={me}
+          alt=""
+          loading="lazy"
+          className="h-40 w-40 object-cover rounded"
+        />
+      </div>
+      <div>
+        <h2 className="pb-5 border-b">Titthei 👋🏼</h2>
+        <div className="overflow-hidden rounded-full border h-40 w-40 sm:hidden block my-10 mx-auto">
+          <Image
+            src={me}
+            alt=""
+            loading="lazy"
+            className="h-40 w-40 object-cover rounded"
+          />
+        </div>
+        <p className="pb-5 sm:pt-5 sm:border-none border-b">
+          Mitt navn er Johanne, og eg studerer informatikk; datatryggleik ved
+          Universitetet i Bergen 👩🏼‍💻.
+        </p>
+      </div>
     </div>
   );
 }
