fe24ff3bbc53f4bbf5b66a7a3805bef9547b3c28
Johanne <joher2128@uib.no>
2023-12-14 23:50:00 +0100

    small føkup

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,42 +1,7 @@
-import Link from "next/link";
-import React from "react";
-
-export const projects = [
-  {
-    title: "echo Webkom",
-    link: "https://beta.echo-webkom.no",
-    description: "Undergruppe i linjeforeningen echo drifter nettsidene til echo"
-  },
-  {
-    title: "Bildegalleri",
-    link: "johannesbilder.vercel.app",
-  },
-  /* {
-    title: "Pokedex",
-    link: "/pokedex",
-  }, */
-];
-
-export function ProjectCard({ title, link }: { title: string; link: string }) {
-  return (
-    <div className="bg-secondary p-3 m-5 w-52">
-      <h3>{title}</h3>
-      <Link href={link}>{title}</Link>
-    </div>
-  );
-}
+import React from 'react'
 
 export default function ProjectPage() {
   return (
-    <div className=" w-screen h-2/3 sm:w-1/3">
-      <div>
-
-      {projects.map((p) => (
-        <div key={p.title} className="flex justify-center">
-          <ProjectCard title={p.title} link={p.link} />
-        </div>
-      ))}
-      </div>
-    </div>
-  );
+    <div className='flex mx-auto justify-center'>coming soon ...</div>
+  )
 }
