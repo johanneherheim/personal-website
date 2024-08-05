0d89145dd58690a25d4b6791618be0847b100b66
Johanne Blikberg Herheim <joher2128@uib.no>
2024-08-05 19:11:20 +0200

    Add navigation

---
+++ app/routes.ts
@@ -0,0 +1,23 @@
+type route = {
+  no: string;
+  en: string;
+  slug: string;
+};
+
+export const routes: route[] = [
+  {
+    no: "Prosjekter",
+    en: "Projects",
+    slug: "projects",
+  },
+  {
+    no: "Spill",
+    en: "Games",
+    slug: "games",
+  },
+  {
+    no: "Verktøy",
+    en: "Tools",
+    slug: "tools",
+  },
+];
---
+++ components/header.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { MenuIcon, X } from "lucide-react";
+import { routes } from "@/app/routes";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex justify-between p-4 border-b items-center relative">
+      <a href="/">
+        <h2 className="uppercase bg-clip-text text-transparent bg-gradient-to-tl from-primary to-secondary hidden sm:block">
+          Johanne Blikberg Herheim
+        </h2>
+        <h2 className="bg-clip-text text-transparent bg-gradient-to-tl from-primary to-secondary block sm:hidden">
+          JBH
+        </h2>
+      </a>
+      <div className="md:hidden">
+        <Button onClick={toggleMenu} size="icon" variant="ghost">
+          <MenuIcon />
+        </Button>
+      </div>
+      <div className="hidden md:flex space-x-5">
+        {routes.map((route) => (
+          <a key={route.en} href={`/${route.slug}`} className="hover:underline">
+            {route.no}
+          </a>
+        ))}
+      </div>
+
+      {isOpen && (
+        <div className="absolute top-0 left-0 w-full h-screen bg-white z-10 flex flex-col p-4 pl-10 space-y-6 md:hidden">
+          <Button
+            onClick={toggleMenu}
+            size="icon"
+            variant="ghost"
+            className="self-end"
+          >
+            <X />
+          </Button>
+          {routes.map((route) => (
+            <a
+              key={route.en}
+              href={`/${route.slug}`}
+              className="hover:underline text-2xl"
+              onClick={() => setIsOpen(false)}
+            >
+              {route.no}
+            </a>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
