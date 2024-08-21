e79241267b40fa474a7eaddaa277ee280c9d6060
Johanne Blikberg Herheim <joher2128@uib.no>
2024-08-21 23:14:11 +0200

    Maybe finished

--- app/tools/abc-calc.tsx
+++ app/tools/abc-calc.tsx
@@ -1,30 +1,58 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
+import { useState } from "react";
 import { RotateCw } from "lucide-react";
-import { sqrt, complex, Complex } from "mathjs";
 
 export default function AbcCalc() {
   const [a, setA] = useState<string>("");
   const [b, setB] = useState<string>("");
   const [c, setC] = useState<string>("");
-  const [answer, setAnswer] = useState<string>("ingen svar");
+
+  const [answer, setAnswer] = useState<string>("");
 
   const handleReset = () => {
     setA("");
     setB("");
     setC("");
-    setAnswer("feil svar");
+    setAnswer("");
   };
 
-  const handleSolve = () => {};
+  const handleSolve = () => {
+    const A = parseFloat(a);
+    const B = parseFloat(b);
+    const C = parseFloat(c);
+
+    if (A === 0) {
+      setAnswer("a kan ikkje vere 0");
+      return;
+    }
+    if (isNaN(A) || isNaN(B) || isNaN(C)) {
+      setAnswer("berre heiltal");
+      return;
+    }
+
+    const discriminant = B ** 2 - 4 * A * C;
+
+    if (discriminant > 0) {
+      const x1 = (-B + Math.sqrt(discriminant)) / (2 * A);
+      const x2 = (-B - Math.sqrt(discriminant)) / (2 * A);
+      setAnswer(`x = ${x1} og x = ${x2}`);
+    } else if (discriminant === 0) {
+      setAnswer(`x = ${-B / (2 * A)}`);
+    } else {
+      const realPart = -B / (2 * A);
+      const imaginaryPart = (Math.sqrt(-discriminant) / (2 * A)).toFixed(3);
+      setAnswer(
+        `x = ${realPart} + ${imaginaryPart}𝑖 og x = ${realPart} – ${imaginaryPart}𝑖`
+      );
+    }
+  };
 
   return (
-    <div className="flex flex-col items-center bg-muted rounded-lg p-5">
-      <h1 className="text-2xl font-bold">ABC Calculator</h1>
-      <p>fungerer ikkje endå, komplekse drittall</p>
+    <div className="flex flex-col items-center bg-muted rounded-lg m-5 p-5 sm:w-1/4 text-center">
+      <h1 className="text-2xl font-bold">ABC Kalkulator</h1>
       <div className="font-serif my-5">
-        p(x) =
+        𝑓(x) =
         <input
           type="text"
           placeholder="a"
@@ -66,7 +94,7 @@ export default function AbcCalc() {
           Rekn ut
         </Button>
       </div>
-      <div className="mt-5">{answer}</div>
+      <div className="mt-5 font-serif">{answer}</div>
     </div>
   );
 }
